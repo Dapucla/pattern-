e17c1a06a6d0876b7a17e4a86a110389a35c1883
Daniil Alekseev <dapcula08@yandex.ru>
2020-04-10 01:51:43 +0300

    Blind adpted

---
+++ index.js
@@ -0,0 +1,136 @@
+$('#normal').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Back page');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#home').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Title');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#about').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('About us');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+
+
+$('#items').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Preferences');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#mainHeader').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Menu');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#aboutus').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('About us');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#aboutUsText').click(function () {
+   var snt = window.speechSynthesis;
+   // mess = new SpeechSynthesisUtterance('Lorem ipsum dolor sit amet consectetur adip totamilique quibusdam rem tenetur sed voluptatibus eveniettotamilique quibusdam rem tenetur sed voluptatibus eveniettotamilique quibusdam rem tenetur sed voluptatibus eveniettotamilique quibusdam rem tenetur sed voluptatibus eveniet');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#prefTitle').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('What we do');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+
+//???????????
+$('#item1').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Design');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#item2').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Program');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#item3').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Upgrade');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#whatwedo').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Our experts');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#phil').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Our Programmer Philip');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#masha').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Our Director Jane');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#natasha').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Our Designer Kate');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#foot').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Contacts');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#facebook').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Facebook');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#vk').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Vk');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
+
+$('#inst').click(function () {
+   var snt = window.speechSynthesis,
+      mess = new SpeechSynthesisUtterance('Instagram');
+   mess.lang = 'en-US';
+   snt.speak(mess);
+});
